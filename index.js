561a18592c48d5a3d1a38d565a3b2f0c9f56a295
TelmuuM <telmuunmunkhzorig@gmail.com>
2020-08-28 16:08:26 +0800

    index to app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Region from './src/Screens/Toirguud/Regions';
-import Dewshigch from './src/Screens/Toirguud/Dewshigch'
+import Dewshigch from './src/Screens/Toirguud/Dewshigch';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Dewshigch);
+AppRegistry.registerComponent(appName, () => App);
